a6148a7eb5b785036c843256cd5cdaea63e4b835
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-07-24 20:57:25 +0530

    Step 53: decode_generated_sequence

--- model.py
+++ model.py
@@ -372,8 +372,10 @@ def generate_sequence(p_matrix, start_id, length, rng):
 
     return np.array(sequence, dtype=np.int64)
 
-# Step 53 - decode_generated_sequence (not yet solved)
-# TODO: implement
+# Step 53 - decode_generated_sequence
+def decode_generated_sequence(ids, itos):
+    """Decode a generated 1D array/list of token ids into a string via itos."""
+    return "".join([itos[id_] for id_ in ids])
 
 # Step 54 - log_prob_of_pair (not yet solved)
 # TODO: implement
