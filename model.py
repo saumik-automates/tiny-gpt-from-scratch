0cae8be6629691f82c0242bc45f6195fa662227e
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-08-01 17:56:10 +0530

    Step 60: forward_logits_onehot

--- model.py
+++ model.py
@@ -418,8 +418,9 @@ def one_hot_encode_batch(ids, vocab_size):
     result[np.arange(N), ids] = 1
     return result
 
-# Step 60 - forward_logits_onehot (not yet solved)
-# TODO: implement
+# Step 60 - forward_logits_onehot
+def forward_logits_onehot(onehot, w_matrix):
+    return matmul(onehot, w_matrix)
 
 # Step 61 - observe_lookup_equivalence (not yet solved)
 # TODO: implement
