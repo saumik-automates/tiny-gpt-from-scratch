371c5ab3d2d6dc6f87952bef20c4a1852e416be9
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-08-01 17:40:23 +0530

    Step 57: initialize_w_random

--- model.py
+++ model.py
@@ -394,8 +394,12 @@ def sum_negative_log_probs(p_matrix, data):
 def average_nll(p_matrix, data):
     return sum_negative_log_probs(p_matrix, data)/(len(data)-1)
 
-# Step 57 - initialize_w_random (not yet solved)
-# TODO: implement
+# Step 57 - initialize_w_random
+import numpy as np
+
+def initialize_w_random(vocab_size, rng):
+    """Return a (vocab_size, vocab_size) float64 matrix of N(0,1) samples drawn from rng."""
+    return rng.standard_normal((vocab_size, vocab_size))
 
 # Step 58 - scale_w_small (not yet solved)
 # TODO: implement
