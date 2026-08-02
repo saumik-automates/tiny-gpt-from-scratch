825a6ee98f323770daa3d3e26935e7ff4c4e88e6
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-08-02 20:23:28 +0530

    Step 62: forward_logits_lookup

--- model.py
+++ model.py
@@ -435,8 +435,10 @@ def observe_lookup_equivalence(w, ids):
             "index_result": w[ids]
            }
 
-# Step 62 - forward_logits_lookup (not yet solved)
-# TODO: implement
+# Step 62 - forward_logits_lookup
+def forward_logits_lookup(w, ids):
+    """Return logits (B, V) by gathering rows of w at positions ids."""
+    return w[ids]
 
 # Step 63 - logits_to_probs_rowwise (not yet solved)
 # TODO: implement
