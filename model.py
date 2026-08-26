ff0d5d98a62f19dfd9820526486da2c937ca3afb
Saumik Kumar Dey <60343919+saumik-automates@users.noreply.github.com>
2026-08-26 20:11:03 +0530

    Step 91: layernorm_backward_implementation

--- model.py
+++ model.py
@@ -702,8 +702,9 @@ def layernorm_backward_full(dy, cache):
     
     return result
 
-# Step 91 - layernorm_backward_implementation (not yet solved)
-# TODO: implement
+# Step 91 - layernorm_backward_implementation
+def layernorm_backward_implementation(d_out, cache):
+    return layernorm_backward_full(d_out, cache)
 
 # Step 92 - create_token_embedding (not yet solved)
 # TODO: implement
